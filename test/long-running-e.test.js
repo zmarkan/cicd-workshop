f5cc5d3eb5ced2481d1642896050c89550019cb6
Zan Markan <zan.markan@gmail.com>
2022-02-24 09:20:36 +0000

    longer tests hahaha

--- test/long-running-e.test.js
+++ test/long-running-e.test.js
@@ -5,50 +5,50 @@ describe("Long running test file E", function() {
     this.timeout(0);
     it("Should wait for 2 seconds 1", async () => {
 
-        await delay(randomValue(2000))
+        await delay(randomValue(5000))
 
         assert.ok(true)
     })
     it("Should wait for 2 seconds 2", async () => {
 
-        await delay(randomValue(2000))
+        await delay(randomValue(5000))
 
         assert.ok(true)
     })
 
     it("Should wait for 2 seconds 3", async () => {
 
-        await delay(randomValue(2000))
+        await delay(randomValue(5000))
 
         assert.ok(true)
     })
     it("Should wait for 2 seconds 4", async () => {
 
-        await delay(randomValue(2000))
+        await delay(randomValue(5000))
 
         assert.ok(true)
     })
     it("Should wait for 2 seconds 5", async () => {
 
-        await delay(randomValue(2000))
+        await delay(randomValue(5000))
 
         assert.ok(true)
     })
     it("Should wait for 2 seconds 6", async () => {
 
-        await delay(randomValue(2000))
+        await delay(randomValue(5000))
 
         assert.ok(true)
     })
     it("Should wait for 2 seconds 7", async () => {
 
-        await delay(randomValue(2000))
+        await delay(randomValue(5000))
 
         assert.ok(true)
     })
     it("Should wait for 2 seconds 8", async () => {
 
-        await delay(randomValue(2000))
+        await delay(randomValue(5000))
 
         assert.ok(true)
     })
--- test/long-running-l.test.js
+++ test/long-running-l.test.js
@@ -5,57 +5,57 @@ describe("Long running test file L", () => {
 
     it("Should wait for a second 1", async () => {
 
-        await delay(randomValue(1000))
+        await delay(randomValue(10000))
 
         assert.ok(true)
     })
 
     it("Should wait for a second 2", async () => {
 
-        await delay(randomValue(1000))
+        await delay(randomValue(10000))
 
         assert.ok(true)
     })
 
     it("Should wait for a second 3", async () => {
 
-        await delay(randomValue(1000))
+        await delay(randomValue(10000))
 
         assert.ok(true)
     })
     it("Should wait for a second 4 ", async () => {
 
-        await delay(randomValue(1000))
+        await delay(randomValue(10000))
 
         assert.ok(true)
     })
     it("Should wait for a second 5 ", async () => {
 
-        await delay(randomValue(1000))
+        await delay(randomValue(10000))
 
         assert.ok(true)
     })
     it("Should wait for a second 6 ", async () => {
 
-        await delay(randomValue(1000))
+        await delay(randomValue(10000))
 
         assert.ok(true)
     })
     it("Should wait for a second 9", async () => {
 
-        await delay(randomValue(1000))
+        await delay(randomValue(10000))
 
         assert.ok(true)
     })
     it("Should wait for a second 7 ", async () => {
 
-        await delay(randomValue(1000))
+        await delay(randomValue(10000))
 
         assert.ok(true)
     })
     it("Should wait for a second 8", async () => {
 
-        await delay(randomValue(1000))
+        await delay(randomValue(10000))
 
         assert.ok(true)
     })
