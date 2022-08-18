9d8e841ea156908797b73ebc4a34c2b208f4e88c
Zan Markan <zan.markan@gmail.com>
2022-08-18 13:56:47 +0100

    remove flaky and redo the workflow

--- test/flaky-test-example.test.js
+++ test/flaky-test-example.test.js
@@ -4,11 +4,11 @@ describe("Flaky sample!", function() {
 
   describe("A flaky test", () => {
 
-    it("works every time, 60% of the time", () => {
-      let dice = Math.random()
-      let testedValue = dice >= 0.4 ? true : false
+    // it("works every time, 60% of the time", () => {
+    //   let dice = Math.random()
+    //   let testedValue = dice >= 0.4 ? true : false
   
-      assert.ok(testedValue, `Dice roll  less than 0.4 ${dice}`)
-    })
+    //   assert.ok(testedValue, `Dice roll  less than 0.4 ${dice}`)
+    // })
   })
 });
