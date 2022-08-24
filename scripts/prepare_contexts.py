82b28e21049dedaa4d262249f61302affc565115
Zan Markan <zan.markan@gmail.com>
2022-08-24 14:04:31 +0000

    scripting token upload logic

--- scripts/prepare_contexts.py
+++ scripts/prepare_contexts.py
@@ -2,55 +2,57 @@
 import toml
 import requests
 import json
+import http.client
 
 creds = toml.load('credentials.toml').get('keys')
-print(creds)
-
-# print(creds.circleci_token)
-# print(creds["circleci_token"])
-# print(creds.get('keys').get('circleci_token'))
-
-circleci_token = creds.get('circleci_token')
-circleci_org_slug = creds.get('circleci_org_slug')
+CIRCLE_TOKEN = creds.get('circleci_token')
+CIRCLECI_ORG_SLUG = creds.get('circleci_org_slug')
+CIRCLECI_ORG_ID = creds.get('circleci_org_id')
+CIRCLECI_BASE_URL = "http://circleci.com/api/v2"
+CIRCLECI_CONTEXT_NAME = "cicd-workshop"
 
 docker_login = creds.get('docker_login')
 docker_pw = creds.get('docker_pw')
 
 
-CIRCLECI_BASE_URL = "http://circleci.com/api/v2"
-CIRCLECI_CONTEXT_NAME = "cicd-workshop"
+# CIRCLECI_CONTEXT_ID = creds.get('circleci_context_id')
+SNYK_TOKEN = creds.get('snyk_token')
+DOCKER_TOKEN = creds.get('docker_token')
+TF_CLOUD_KEY = creds.get('tf_cloud_key')
+DIGITALOCEAN_TOKEN = creds.get('digitalocean_token')
 
+# Assuming we always return JSON
+def circleci_api_request(method, endpoint, payload_dict):
+  conn = http.client.HTTPSConnection("circleci.com")
+  headers = {
+    'content-type': "application/json",
+    'Circle-Token': CIRCLE_TOKEN
+  }
+  conn.request(method, f'/api/v2/{endpoint}', json.dumps(payload_dict), headers)
+  
+  res = conn.getresponse()
+  data = res.read()
+  data_str = data.decode("utf-8")
+  return json.loads(data_str)
 
-payload = {
+def add_circle_token_to_context(context_id, env_var_name, env_var_value):
+  return circleci_api_request("PUT", f'context/{context_id}/environment-variable/{env_var_name}', { "value": env_var_value })
+
+# Create CircleCI context and get ID
+context_payload = {
     "name": CIRCLECI_CONTEXT_NAME,
-    "owner": {
-        "slug": circleci_org_slug,
+      "owner": {
+        "id": CIRCLECI_ORG_ID,
         "type": "organization"
-    }
+      }
 }
 
+context_res = circleci_api_request("POST", f'context', context_payload)
+circleci_context_id = context_res.get("id")
+# TODO: check if the context already exists...
 
-# payload = "{\n    \"name\": {{CIRCLE_CONTEXT_NAME}},\n    \"owner\": {\n        \"slug\": {{ORG_SLUG}},\n        \"type\": \"organization\"\n    }\n}"
-headers = {
-  'Circle-Token': circleci_token,
-  'Content-Type': 'application/json'
-}
-
-print(payload)
-# print(headers)
-
-response = requests.request("POST", CIRCLECI_BASE_URL+"/context", headers=headers, json=payload)
-
-print(response.status_code)
-print(response.json())
-
-# Get CircleCI org type?
-# Create Context for CircleCI
-
-# Create token for Docker
-
-# Add env variable for Snyk token
-# Add env variable for Docker token
-# Add env variable for DO token
-
-# Add env variable(s) for AWS creds
+# Add Env vars to context
+print(add_circle_token_to_context(circleci_context_id, "SNYK_TOKEN", SNYK_TOKEN))
+print(add_circle_token_to_context(circleci_context_id, "DOCKER_TOKEN", DOCKER_TOKEN))
+print(add_circle_token_to_context(circleci_context_id, "TF_CLOUD_KEY", TF_CLOUD_KEY))
+print(add_circle_token_to_context(circleci_context_id, "DIGITALOCEAN_TOKEN", DIGITALOCEAN_TOKEN))
